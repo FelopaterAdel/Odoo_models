388532720877d8298055a26cb5579a0d26df4f2d
FelopaterAdel <felopateradel@yahoo.com>
2025-06-03 21:55:14 +0300

    security

--- custom_addons/hms/__manifest__.py
+++ custom_addons/hms/__manifest__.py
@@ -4,13 +4,16 @@
 
     'version': '1.0',
     'summary': 'Manage Hospital Patients',
-    'depends': ['base'],
+    'depends': ['base','crm'],
     'data': [
+        "security/ir.model.access.csv",
         'views/patient_view.xml',
         'views/menu.xml',
         'views/department_view.xml',
         'views/doctors_view.xml',
+        "views/patient_log_view.xml",
         'views/res_partner_view.xml',
     ],
-
+    "application": True,
+    "installable": True,
 }
